cd2dfa0affa56bcd3eeb7c175aa4742dd5876872
white0527 <white0527@example.com>
2026-02-13 21:53:57 +0800

    Final cleanup: Project is clean and ready

--- src/main/java/com/example/demo/repository/SalesOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.SalesOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalesOrderRepository extends JpaRepository<SalesOrder, Long> {
-}
